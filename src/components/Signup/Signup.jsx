1cc86a43628c59e902d60d412a687578287ad931
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-26 14:55:17 +0600

    create login and sign up component and style them

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -3,8 +3,23 @@ import './Signup.css'
 
 const Signup = () => {
     return (
-        <div>
-            <h2>Please Sign ups</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Please Sign up</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Confirm Password</label>
+                    <input type="password" name="confirm-password" id="" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign up" />
+            </form>
         </div>
     );
 };
